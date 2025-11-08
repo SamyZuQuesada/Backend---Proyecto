5f972477a84efc71fadf5164198591092ef1d2fa
Gvrh05 <Gvrh05@users.noreply.github.com>
2025-11-08 15:34:48 -0600

    cambios en el dto(IsOpt)

--- src/testimonios/DTO/update-testimonio.dto.ts
+++ src/testimonios/DTO/update-testimonio.dto.ts
@@ -1,9 +1,12 @@
 import { IsString } from 'class-validator'; //hay que ejecutar  "npm install class-validator class-transformer para que esto funcione"
+import { IsOptional } 
 
 export class UpdateTestimonioDto {
+  @IsOptional()
   @IsString() //si las validaciones siguen dando errores se pueden borrar
-  name: string;
+  name?: string;
 
+  @IsOptional()
   @IsString()
-  description: string;
+  description?: string;
 }
